64a8b4d3c1e4dd6472e543277aee3a6fd0111e1e
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2019-12-04 21:35:59 -0500

    working on chapter 3

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     let g = temp_convert(32.0, 'f');
-    println!("The sum is {}", g)
+    println!("The sum is {}", g);
+
+    for n in 0..20 {
+        println!("The fibonacci number for {} is {}", n, fibonacci(n));
+    }
 }
 
 fn temp_convert(temperature: f32, base: char) -> f32 {
@@ -11,5 +15,17 @@ fn temp_convert(temperature: f32, base: char) -> f32 {
     } else {
         println!("Only conversions between Celsuis (C) and Fahnrenheit (F) are possible.");
         -9999.0
+    }
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else if n > 1 {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    } else {
+        panic!("n must be a unsigned integer!")
     }
 }
