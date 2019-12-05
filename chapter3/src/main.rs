60da2009e51c229d2081cfc9f4c8710e11a4e6ca
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2019-12-04 21:09:57 -0500

    working on chapter 3

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,23 +1,15 @@
 fn main() {
-    let x = 5;
-    println!("The value of x is {}", x);
-
-    another_fn(x);
-
-    a_second_fn(5, 33);
-
-    let g = a_third_fn(55.0, 32.0);
+    let g = temp_convert(32.0, 'f');
     println!("The sum is {}", g)
 }
 
-fn another_fn(x: u32) {
-    println!("The variable passed to the function is {}", x);
-}
-
-fn a_second_fn(x: u32, y: u32) {
-    println!("You provided {} and {}", x, y);
-}
-
-fn a_third_fn(x: f32, y: f32) -> f32 {
-    x + y
+fn temp_convert(temperature: f32, base: char) -> f32 {
+    if base == 'C' {
+        temperature * (9.0 / 5.0) + 32.0
+    } else if base == 'F' {
+        (temperature - 32.0) * (5.0 / 9.0)
+    } else {
+        println!("Only conversions between Celsuis (C) and Fahnrenheit (F) are possible.");
+        -9999.0
+    }
 }
