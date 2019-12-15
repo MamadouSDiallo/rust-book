a8d677d4c087098b4405bf83f3e8d20621d8e73c
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2019-12-14 22:35:56 -0500

    working on chapter 6

--- chapter6/src/main.rs
+++ chapter6/src/main.rs
@@ -1,3 +1,81 @@
+#[allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let m = Message::Write(String::from("Hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    //let absent_number: std::option::Option<i32> = None;
+
+    let some_number_plus_one = plus_one(some_number);
+    println!(
+        "Some number {:?} plus one is equal to {:?}",
+        some_number, some_number_plus_one
+    );
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("The method will be implemented here");
+    }
+}
+
+fn plus_one(i: Option<i32>) -> Option<i32> {
+    match i {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
