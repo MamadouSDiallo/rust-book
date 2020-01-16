18c20e997b4f279b533caf6b276bac41d11dbb4f
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2020-01-16 10:45:21 -0500

    working on chapter 13

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,9 +5,12 @@ use minigrep::run;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let _args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap();
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
